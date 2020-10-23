f91aee5f738e97f187a148ffd3ac60977be30e80
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-10-22 19:35:02 -0700

    added prompts and confirms for character types

--- script.js
+++ script.js
@@ -11,17 +11,36 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 // upper case array 
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var generateBtn = document.querySelector("generate");
+var generateBtn = document.querySelector("#generate");
 
+
+// prompts questions when generate password is clicked
 function generatePassword() {
   // password length
-  var pwordLength = prompt("Choose password length, must be between 8 and 128 characters");
+  var pwrdLength = prompt("Choose password length, must be between 8 and 128 characters");
+  
+  var speChar = confirm("Would you like to use special characters for your password")
 
+  var number = confirm("Would you like to use numbers for your password");
+
+  var lowerCase = confirm("Would you like to use lowercase letters for your password");
+
+  var upperCase = confirm("Would you like to use uppercase letters for your password");
+
+
+   
+var password = "";
+while (password.length < pwrdLength){
+
+}
+return password;
 };
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
 }
 
