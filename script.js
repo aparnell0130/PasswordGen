2f03f228220379e5ab8746c519bbaa9e15fadc75
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-10-23 22:01:19 -0700

    added more special characters to array

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Assignment Code
  // special character array
- var speChars = ["!","@","#","$","%","^","&","*","(",")"]; 
+ var speChars = ["!","@","#","$","%","^","&","*","(",")","`","<",">","?","~","[","]","{","}","|","/","+","-"]; 
 
 // number array
 var numbers = ["1","2","3","4","5","6","7","8","9"];
 
-// lower case array
+// lowercase array
 var lowerCases = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-// upper case array 
+// uppercase array 
 var upperCases = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 var generateBtn = document.querySelector("#generate");
